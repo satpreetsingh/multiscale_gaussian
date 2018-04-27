0f79f3672d70ce0bcdefa7113b85d5f793e5aa5e
Josue <josue.nassar@stonybrook.edu>
2018-04-27 18:32:18 -0400

    Rewrote gradient function for locations of clusters

--- gradients.py
+++ gradients.py
@@ -206,6 +206,7 @@ def like_gradient(alpha1,alpha2,alpha3,alpha4,alpha5,alpha6,sigma1,sigma2,sigma3
     return a1.grad.data,a2.grad.data,a3.grad.data,a4.grad.data,a5.grad.data,a6.grad.data,s1.grad.data,s2.grad.data,s3.grad.data,s4.grad.data,s5.grad.data,s6.grad.data
 
 def like_gradient_mu(dim,alpha,beta,sigma,nu,e_p,e_k,Inv_Cov_p,Inv_Cov_k,LDS_p,LDS_k,x_prev,x_curr,u_prev,IQ,NumParents,NumKids,PerParent):
+    
     #Variables we don't need gradient of 
     IC_p=Variable(torch.from_numpy(Inv_Cov_p).float()) #Covariance matrices of parents
     IC_k=Variable(torch.from_numpy(Inv_Cov_k).float()) #Covariance matrices of kids
@@ -215,38 +216,49 @@ def like_gradient_mu(dim,alpha,beta,sigma,nu,e_p,e_k,Inv_Cov_p,Inv_Cov_k,LDS_p,L
     x_p=Variable(torch.from_numpy(x_prev).float()) #x_{t-1}
     x_c=Variable(torch.from_numpy(x_curr).float()) #x_t
     u=Variable(torch.from_numpy(u_prev).float()) #u=[x_{t-1};1]
-    ep=Variable(torch.from_numpy(e_p).float())
-    ek=Variable(torch.from_numpy(e_k).float())
-    x_temp=Variable(torch.from_numpy(np.matrix(np.zeros(dim)).T).float())
+    ep=Variable(torch.from_numpy(np.matrix(e_p)).float())
+    ek=Variable(torch.from_numpy(np.matrix(e_k)).float())
     
     #Variables that we do need the gradients of 
-    a=Variable(torch.from_numpy(alpha).float(),requires_grad=True)
+    a=Variable(torch.from_numpy(np.matrix(alpha)).float(),requires_grad=True)
     a.retain_grad()
-    b=Variable(torch.from_numpy(beta).float(),requires_grad=True)
+    b=Variable(torch.from_numpy(np.matrix(beta)).float(),requires_grad=True)
     b.retain_grad()
-    s=Variable(torch.from_numpy(sigma).float(),requires_grad=True)
+    s=Variable(torch.from_numpy(np.matrix(sigma)).float(),requires_grad=True)
     s.retain_grad()
-    n=Variable(torch.from_numpy(nu).float(),requires_grad=True)
+    n=Variable(torch.from_numpy(np.matrix(nu)).float(),requires_grad=True)
     n.retain_grad()
     
     #Used to store weights
     weights_parents=Variable(torch.from_numpy(np.matrix(np.zeros(NumParents)).T).float())
     weights_kids=Variable(torch.from_numpy(np.matrix(np.zeros(NumKids)).T).float())
-    
+    x_temp_parents=Variable(torch.from_numpy(np.matrix(np.zeros((dim,NumParents)))).float())
+    x_temp_kids=Variable(torch.from_numpy(np.matrix(np.zeros((dim,NumKids)))).float())
+
+
     #Weigh Clusters
     for p in range(0,NumParents):
-        weights_parents[p]=torch.exp(torch.matmul(torch.matmul(-(x_p-a[:,p]-s[p]*ep[:,p]).transpose(0,1),IC_p[:,:,p]),(x_p-a[:,p]-s[p]*ep[:,p])))+1e-50
-        x_temp+=weights_parents[p]*torch.matmul(Theta_p[:,:,p],u)
+        weights_parents[p,0]=torch.exp( torch.matmul( torch.matmul( -(x_p[:,0]-a[:,p]-s[:,p]*ep[:,p]).unsqueeze(0),IC_p[:,:,p]) ,(x_p[:,0]-a[:,p]-s[:,p]*ep[:,p]))   )+1e-50
+        x_temp_parents[:,p]=(torch.exp( torch.matmul( torch.matmul( -(x_p[:,0]-a[:,p]-s[:,p]*ep[:,p]).unsqueeze(0),IC_p[:,:,p]) ,(x_p[:,0]-a[:,p]-s[:,p]*ep[:,p]))   )+1e-50)*torch.matmul(Theta_p[:,:,p],u)
         
     for k in range(0,NumKids):
         p=int(k/PerParent)
-        weights_kids[k]=torch.exp(torch.matmul(torch.matmul(-(x_p-a[:,p]-s[p]*ep[:,p]-b[:,k]-n[k]*ek[:,k]).transpose(0,1),IC_k[:,:,k]),(x_p-a[:,p]-s[p]*ep[:,p]-b[:,k]-n[k]*ek[:,k])))+1e-50
-        x_temp+=weights_kids[p]*torch.matmul(Theta_k[:,:,k],u)
+        weights_kids[k,0]=torch.exp(torch.matmul(torch.matmul(-(x_p[:,0]-a[:,p]-s[:,p]*ep[:,p]-b[:,k]-n[:,k]*ek[:,k]).unsqueeze(0),IC_k[:,:,k]),(x_p[:,0]-a[:,p]-s[:,p]*ep[:,p]-b[:,k]-n[:,k]*ek[:,k])))+1e-50
+        x_temp_kids[:,k]=(torch.exp(torch.matmul(torch.matmul(-(x_p[:,0]-a[:,p]-s[:,p]*ep[:,p]-b[:,k]-n[:,k]*ek[:,k]).unsqueeze(0),IC_k[:,:,k]),(x_p[:,0]-a[:,p]-s[:,p]*ep[:,p]-b[:,k]-n[:,k]*ek[:,k])))+1e-50)*torch.matmul(Theta_k[:,:,k],u)
+    
+
     #Normalize
     Z=torch.sum(weights_parents)+torch.sum(weights_kids)
-    x_temp=x_temp/Z
+    x_p_clone=x_temp_parents.clone()
+    x_k_clone=x_temp_kids.clone()
+    x_unnorm=torch.sum(x_p_clone,1)+torch.sum(x_k_clone.clone(),1)
+    x_norm=x_unnorm.clone()/Z
     
-    L=-0.5*torch.matmul(torch.matmul((x_c-x_temp).transpose(0,1),IQ),(x_c-x_temp))
+    y=x_norm.clone().unsqueeze(1)
+    z=x_c-y
+    F=torch.matmul(torch.matmul((z).transpose(0,1),IQ),(z))
+    L=-0.5*F
+
     L.backward()
     return a.grad.data,b.grad.data,s.grad.data,n.grad.data
     
